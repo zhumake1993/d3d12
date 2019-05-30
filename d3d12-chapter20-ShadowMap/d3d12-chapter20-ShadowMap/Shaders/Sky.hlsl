ea21fda863849c8bc990a1b04c2ec3af46656a43
zhumake1993 <zhumake1993@gmail.com>
2019-05-30 11:39:16 +0800

    新增d3d12-chapter20-ShadowMap，代码尚存疑

---
+++ d3d12-chapter20-ShadowMap/d3d12-chapter20-ShadowMap/Shaders/Sky.hlsl
@@ -0,0 +1,37 @@
+#include "Common.hlsl"
+
+struct VertexIn
+{
+	float3 PosL    : POSITION;
+	float3 NormalL : NORMAL;
+	float2 TexC    : TEXCOORD;
+};
+
+struct VertexOut
+{
+	float4 PosH : SV_POSITION;
+    float3 PosL : POSITION;
+};
+ 
+VertexOut VS(VertexIn vin)
+{
+	VertexOut vout;
+
+	vout.PosL = vin.PosL;
+	
+	float4 posW = mul(float4(vin.PosL, 1.0f), gWorld);
+
+	// 以摄像机为中心
+	posW.xyz += gEyePosW;
+
+	// 设z = w，使得z/w = 1 (天空在远平面).
+	vout.PosH = mul(posW, gViewProj).xyww;
+	
+	return vout;
+}
+
+float4 PS(VertexOut pin) : SV_Target
+{
+	return gCubeMap.Sample(gsamLinearWrap, pin.PosL);
+}
+
